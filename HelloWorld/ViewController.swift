aaa6ff9dd3c0c3bb23e79f7563001495a74adba1
msandlarz <msandlarz1@gmail.com>
2015-09-19 15:45:46 -0400

    Initial Commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "http://goldsand.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
